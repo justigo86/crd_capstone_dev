6222db1a840d7b618afe878990d9f84f2c5dbbff
Otto <justigo86@gmail.com>
2023-04-15 21:36:35 -0400

    added firebase to app - and links/routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
-
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+const Shop = () => {
+  return <h1>shop</h1>;
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />}/>
+        <Route path='shop' element={<Shop />}/>
+        <Route path='signIn' element={<SignIn />}/>
+      </Route>
     </Routes>
   );
 };
